79d7907c1156a3759d4bf9423073fabea80710fc
DrError <daniss404@gmx.com>
2024-09-23 12:36:13 +0300

    Task nr.1

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # LLVSK_11
+
+
+
+
+
+
+
+
+### TODO:
+
+- []  nokolēt repository uz datora
+- []  atjaunināt repository ja ir vecāka versija (Fetch)
+- []  salabot/debug kodu
+- []  palaist kodu
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # Pieprasām lietotāja vārdu un vecumu
 name = input("Kāds ir tavs vārds? ")
-age = int(input("Cik tev gadu? "))
+age = input("Cik tev gadu? ")
 
 # Izvadām vārdu un vecumu
 print(f"Tevi sauc {name}, un tev ir {age} gadi.")
 
 # Veicam nelielu matemātiku - aprēķinām dzimšanas gadu
 current_year = 2024
-birth_year = current_year - age
+birth_year = current_year / age
 
 # Izvadām aprēķināto dzimšanas gadu
 print(f"Tavs dzimšanas gads ir aptuveni {birth_year}.")
