cb6d0befc3abdbfb5a9cdb5d4e60e28a5845281e
DrError <daniss404@gmx.com>
2024-09-30 12:53:30 +0300

    task 1 mistakes fixed

--- main.py
+++ main.py
@@ -7,19 +7,19 @@
 #
 # Pieprasām lietotāja vārdu un vecumu
 name = input("Kāds ir tavs vārds? ")
-age = input("Cik tev gadu? ")
+age = int(input("Cik tev gadu? "))
 
 # Izvadām vārdu un vecumu
 print(f"Tevi sauc {name}, un tev ir {age} gadi.")
 
-if age >= 18
-    print("Tu neesi pieaudzis!")
-else:
+if age >= 18:
     print("Tu esi pieaudzis!")
+else:
+    print("Tu neesi pieaudzis!")
 
 # Veicam nelielu matemātiku - aprēķinām dzimšanas gadu
 current_year = 2024
-birth_year = current_year / age
+birth_year = current_year - age
 
 # Izvadām aprēķināto dzimšanas gadu
 print(f"Tavs dzimšanas gads ir aptuveni {birth_year}.")
